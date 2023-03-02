61da4fca64ce9eb441bbfabc7c6d72e65a4a0c46
saititests <116989300+saititests@users.noreply.github.com>
2023-03-02 23:09:59 +0200

    Update TestsWithoutWhiteNoise.tsx

--- src/components/TestsWithoutWhiteNoise.tsx
+++ src/components/TestsWithoutWhiteNoise.tsx
@@ -1,12 +1,12 @@
 import Test, { TestType } from "./Test";
 
 import { Box } from "@mui/material";
-import { test3Array } from "./audio/soundsData";
+import { test4Array } from "./audio/soundsData";
 
 const TestsWithoutWhiteNoise = () => {
   return (
     <Box sx={{ width: "100%" }}>
-      <Test patientArray={test3Array} type={TestType.Regular} />
+      <Test patientArray={test4Array} type={TestType.Regular} />
     </Box>
   );
 };
